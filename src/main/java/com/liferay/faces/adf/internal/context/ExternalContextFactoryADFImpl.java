0522a8e18b6c7166131663341fd6a07ce29dd08f
Neil Griffin <neil.griffin.scm@gmail.com>
2019-07-02 18:34:10 -0400

    FACES-3460 Support ADF Faces (Move to com.liferay.faces.adf.internal.context sub-package)

--- src/main/java/com/liferay/faces/adf/internal/context/ExternalContextFactoryADFImpl.java
+++ src/main/java/com/liferay/faces/adf/internal/context/ExternalContextFactoryADFImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.liferay.faces.adf.internal;
+package com.liferay.faces.adf.internal.context;
 
 import javax.faces.FacesException;
 import javax.faces.context.ExternalContext;
