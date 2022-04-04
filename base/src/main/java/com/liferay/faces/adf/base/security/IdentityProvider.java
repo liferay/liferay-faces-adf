b5c8824d2e88b8893033abba4dc3334b2ad32217
Neil Griffin <neil.griffin.scm@gmail.com>
2022-04-04 13:35:34 -0400

    FACES-3658 Switch to the LGPL 2.1 license for Liferay Faces ADF

--- base/src/main/java/com/liferay/faces/adf/base/security/IdentityProvider.java
+++ base/src/main/java/com/liferay/faces/adf/base/security/IdentityProvider.java
@@ -1,21 +1,21 @@
 /**
- * Copyright (c) 2000-2021 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2022 Liferay, Inc. All rights reserved.
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 package com.liferay.faces.adf.base.security;
 
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 
 import oracle.adf.share.security.credentialstore.Credential;
 import oracle.adf.share.security.identitymanagement.AttributeFilter;
@@ -25,7 +25,7 @@ import oracle.adf.share.security.identitymanagement.spi.IdentityManagement;
 
 
 /**
- * Provides a minimial implementation of an ADF identity provider that can be specified in an adf-config.xml descriptor.
+ * Provides a minimal implementation of an ADF identity provider that can be specified in an adf-config.xml descriptor.
  * For example:
  *
  * <pre>
